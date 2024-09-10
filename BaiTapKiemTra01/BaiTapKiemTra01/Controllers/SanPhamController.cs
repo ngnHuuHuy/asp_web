2a650db1579c224de7ef2132ad375bf33f12a078
ngnHuuHuy <huyn46956@gmail.com>
2024-09-10 15:11:30 +0700

    bai kiem tra 01

---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Controllers/SanPhamController.cs
@@ -0,0 +1,24 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BaiTap02()
+        {
+            var sanpham = new SanPhamViewModel()
+            {
+                TenSanPham = "hp laser da nang",
+                GiaBan = 6000,
+                AnhMoTa = "/images/hp-laser-da-nang-laserjet-135a-4zb82a-thumb-600x600.jpg"
+            };
+
+            return View(sanpham);
+        }
+    }
+}
---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Controllers/TaiKhoanController.cs
@@ -0,0 +1,22 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel taikhoan)
+        {
+            if (taikhoan != null && taikhoan.MatKhau !=null && (taikhoan.MatKhau).Length>0)
+            {
+                taikhoan.MatKhau = taikhoan.MatKhau + "_chuoi_ma_hoa";
+            }
+
+            return View(taikhoan);
+        }
+    }
+}
